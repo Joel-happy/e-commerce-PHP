6fc700b5ac6161c709b476b5b2a99a1b546a369d
MIHUTA Mihail <mihail.mihuta@ynov.com>
2024-01-29 10:51:02 +0100

    [ADD] database connection + [UPDATE] hierarchy

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 
-require_once 'router.php';
+require_once 'src/utilities/router.php';
+require_once 'src/utilities/database.php';
 
 // Define the base path
 $basePath = '/e-commerce-PHP';
@@ -11,7 +12,13 @@ $requestPath = filter_var($_SERVER['REQUEST_URI'], FILTER_SANITIZE_URL);
 // Remove the base path from the beginning of the request path and trim '/'
 $requestPath = trim(str_replace($basePath, '', $requestPath), '/');
 
+// Instantiate Database class
+$database = new Database();
+
 // Execute corresponding function for that specific path
 $router->handleRequest($requestPath);
+
+// Close database connection
+$database->close();
 
 ?>
